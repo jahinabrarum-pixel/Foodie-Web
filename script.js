ba59d18424b540daba89461d2fedf8aa204491ad
Abrar <abrarjahin702@gmail.com>
2026-06-24 20:07:17 +0600

    Implement cart functionality and add product data; enhance layout and styling for better user experience

--- script.js
+++ script.js
@@ -20,9 +20,53 @@ document.addEventListener("click", (e) => {
 });
 
 var swiper = new Swiper(".mySwiper", {
-    loop: true,
+  loop: true,
   navigation: {
     nextEl: "#next",
     prevEl: "#prev",
   },
 });
+
+// Start Main JS
+const cartIcon = document.querySelector(".cart-icon");
+const cartTab = document.querySelector(".cart-tab");
+const closeBtn = document.querySelector(".close-btn");
+const cardlist = document.querySelector(".card-list");
+
+cartIcon.addEventListener("click", () =>
+  cartTab.classList.add("cart-tab-active"),
+);
+closeBtn.addEventListener("click", () =>
+  cartTab.classList.remove("cart-tab-active"),
+);
+
+let productList = [];
+
+const showCards = () => {
+  productList.forEach((product) => {
+    const orderCard = document.createElement("div");
+    orderCard.classList.add("order-card");
+
+    orderCard.innerHTML = `
+    <div class="crad-image">
+      <img src="${product.image}">
+    </div>
+    <h4>${product.name}</h4>
+    <h4 class="price">${product.price}</h4>
+    <a href="#" class="btn">Add to Cart</a>
+    `;
+
+    cardlist.appendChild(orderCard);
+  });
+};
+
+const initApp = () => {
+  fetch("products.json")
+    .then((response) => response.json())
+    .then((data) => {
+      productList = data;
+      showCards();
+    });
+};
+
+initApp();
